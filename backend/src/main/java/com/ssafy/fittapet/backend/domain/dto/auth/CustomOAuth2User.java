30a293ca952b925642de31c802f27abdc2dcd0bc
yootal <khyoo96@naver.com>
2024-11-06 03:42:58 +0900

    feat: CustomOAuth2User 작성

---
+++ backend/src/main/java/com/ssafy/fittapet/backend/domain/dto/auth/CustomOAuth2User.java
@@ -0,0 +1,55 @@
+package com.ssafy.fittapet.backend.domain.dto.auth;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 인증된 사용자 정보 저장용
+ * SecurityContextHolder 등록한 후 접근
+ */
+public class CustomOAuth2User implements OAuth2User {
+
+    private final UserDTO userDTO;
+
+    public CustomOAuth2User(UserDTO userDTO) {
+        this.userDTO = userDTO;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("id", userDTO.getUserId());
+        attributes.put("username", userDTO.getUserNickname());
+        attributes.put("role", userDTO.getRole());
+
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add((GrantedAuthority) userDTO::getRole);
+
+        return collection;
+    }
+
+    public Long getId(){
+        return userDTO.getUserId();
+    }
+
+    @Override
+    public String getName() {
+        return userDTO.getUserNickname();
+    }
+
+    public String getRole(){
+        return userDTO.getRole();
+    }
+}
