a096d1ba99cc897aae8d497f3001272816b66b6f
장성일 <cart1231@naver.com>
2024-11-06 10:28:23 +0900

    feat: 지도 모달 1차 완성

--- frontend/src/screens/map/MapModal.jsx
+++ frontend/src/screens/map/MapModal.jsx
@@ -16,28 +16,33 @@ const MapModal = ({
   const [inviteCodeValue, setInviteCodeValue] = useState('');
 
   const handleCreateGroup = () => {
-    // 그룹 생성 로직
-    const groupExists = false; // 예시 로직
+    const groupExists = true; // 예시 로직
     if (groupExists) {
       setErrorState('duplicate');
     } else {
       onCreateGroup();
+      setErrorState(null); // 성공 시 에러 상태 초기화
     }
-    setNewGroupValue();
+    setNewGroupValue(''); // 입력 값 초기화
   };
 
   const handleJoinGroup = () => {
-    // 그룹 참여 로직
     const isCodeValid = true; // 예시 로직
-    const isGroupFull = false; // 예시 로직
+    const isGroupFull = true; // 예시 로직
     if (!isCodeValid) {
       setErrorState('invalidCode');
     } else if (isGroupFull) {
+      console.log('꽉참');
       setErrorState('full');
     } else {
       onJoinGroup();
+      setErrorState(null); // 성공 시 에러 상태 초기화
     }
-    setInviteCodeValue();
+    setInviteCodeValue(''); // 입력 값 초기화
+  };
+
+  const goBack = () => {
+    setErrorState(null);
   };
 
   return (
@@ -50,7 +55,7 @@ const MapModal = ({
           : errorState === 'invalidCode'
           ? '코드 불일치!'
           : errorState === 'full'
-          ? '그룹이 가득 찼습니다!'
+          ? '그룹 인원 제한!'
           : viewState === 'init'
           ? '빈 그룹이네요!'
           : viewState === 'create'
@@ -94,9 +99,18 @@ const MapModal = ({
         <>
           <View style={styles.mapModalBody}>
             {errorState === 'duplicate' ? (
-              <CustomText style={styles.errorText}>
-                이미 존재하는 그룹 이름입니다. 다시 입력해주세요!
-              </CustomText>
+              <>
+                <CustomText style={styles.errorText}>
+                  이미 있는 그룹 이름이네요..
+                </CustomText>
+                <CustomText style={styles.errorText}>
+                  다시 입력해주세요!
+                </CustomText>
+
+                <View style={styles.mapModalBottom}>
+                  <CustomButton title="알겠어" onPress={goBack} />
+                </View>
+              </>
             ) : (
               <>
                 <CustomText>
@@ -109,11 +123,11 @@ const MapModal = ({
                   onChangeText={setNewGroupValue}
                   keyboardType="default"
                 />
+                <View style={styles.mapModalBottom}>
+                  <CustomButton title="이거야" onPress={handleCreateGroup} />
+                </View>
               </>
             )}
-          </View>
-          <View style={styles.mapModalBottom}>
-            <CustomButton title="그룹 생성" onPress={handleCreateGroup} />
           </View>
         </>
       )}
@@ -122,18 +136,35 @@ const MapModal = ({
         <>
           <View style={styles.mapModalBody}>
             {errorState === 'invalidCode' ? (
-              <CustomText style={styles.errorText}>
-                초대 코드가 맞지 않습니다. 다시 입력해주세요!
-              </CustomText>
+              <>
+                <CustomText style={styles.errorText}>
+                  초대 코드가 맞지 않아요!
+                </CustomText>
+                <CustomText style={styles.errorText}>
+                  다시 코드를 입력해봐요!
+                </CustomText>
+                <View style={styles.mapModalBottom}>
+                  <CustomButton title="알겠어" onPress={goBack} />
+                </View>
+              </>
             ) : errorState === 'full' ? (
-              <CustomText style={styles.errorText}>
-                해당 그룹은 이미 가득 찼습니다.
-              </CustomText>
+              <>
+                <CustomText style={styles.errorText}>
+                  해당 그룹은 이미 6명이라
+                </CustomText>
+                <CustomText style={styles.errorText}>못들어가요</CustomText>
+                <CustomText style={styles.errorText}>
+                  다시 코드를 입력해봐요!
+                </CustomText>
+
+                <View style={styles.mapModalBottom}>
+                  <CustomButton title="알겠어" onPress={goBack} />
+                </View>
+              </>
             ) : (
               <>
-                <CustomText>
-                  선택한 그룹에 참여하려면 초대 코드를 아래에 입력해주세요!
-                </CustomText>
+                <CustomText>선택한 그룹에 참여하려면</CustomText>
+                <CustomText>초대 코드를 입력해주세요!</CustomText>
                 <TextInput
                   style={styles.input}
                   placeholder="초대 코드는.."
@@ -141,11 +172,11 @@ const MapModal = ({
                   onChangeText={setInviteCodeValue}
                   keyboardType="default"
                 />
+                <View style={styles.mapModalBottom}>
+                  <CustomButton title="이거야" onPress={handleJoinGroup} />
+                </View>
               </>
             )}
-          </View>
-          <View style={styles.mapModalBottom}>
-            <CustomButton title="그룹 참여" onPress={handleJoinGroup} />
           </View>
         </>
       )}
@@ -158,7 +189,7 @@ const styles = StyleSheet.create({
     width: 280,
   },
   mapModalBottom: {
-    marginTop: 20,
+    marginTop: 10,
     width: 300,
     flexDirection: 'row',
     justifyContent: 'center',
@@ -176,12 +207,16 @@ const styles = StyleSheet.create({
   },
   errorText: {
     color: 'red', // 에러 메시지 스타일
-    fontSize: 16,
-    marginBottom: 10,
   },
   input: {
     fontFamily: 'DungGeunMo',
     fontSize: 16,
+    backgroundColor: 'gray', // 색 변화시킬거
+    paddingLeft: 15,
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: 'transparent',
+    marginTop: 10,
   },
 });
 
