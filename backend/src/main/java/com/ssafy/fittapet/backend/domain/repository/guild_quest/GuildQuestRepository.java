783853c912cba9ea588ab602b0a91bea1a4488b4
남동균 <mark3473@naver.com>
2024-11-06 15:52:18 +0900

    feat : getGuildQuestInfo 개발

--- backend/src/main/java/com/ssafy/fittapet/backend/domain/repository/guild_quest/GuildQuestRepository.java
+++ backend/src/main/java/com/ssafy/fittapet/backend/domain/repository/guild_quest/GuildQuestRepository.java
@@ -3,7 +3,7 @@ package com.ssafy.fittapet.backend.domain.repository.guild_quest;
 import com.ssafy.fittapet.backend.domain.entity.GuildQuest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GuildQuestRepository extends JpaRepository<GuildQuest, Long> {
+public interface GuildQuestRepository extends JpaRepository<GuildQuest, Long>, GuildQuestCustomRepository {
 
     GuildQuest findByGuildId(Long guildId);
 }
