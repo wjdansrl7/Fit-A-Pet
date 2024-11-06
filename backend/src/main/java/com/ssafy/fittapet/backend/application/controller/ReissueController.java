ae09ab5ef3dd3248b9d3bcbe5eaa08a2437f2188
yootal <khyoo96@naver.com>
2024-11-06 14:28:32 +0900

    feat: ReissueController, Service 작성

---
+++ backend/src/main/java/com/ssafy/fittapet/backend/application/controller/ReissueController.java
@@ -0,0 +1,21 @@
+package com.ssafy.fittapet.backend.application.controller;
+
+import com.ssafy.fittapet.backend.application.service.auth.ReissueService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ReissueController {
+
+    private final ReissueService reissueService;
+
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
+        return reissueService.reissueToken(request, response);
+    }
+}
