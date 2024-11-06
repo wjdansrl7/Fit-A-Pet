6b1d40a48ef6b4e28d4309a43e359cd86112047a
yootal <khyoo96@naver.com>
2024-11-06 14:27:46 +0900

    feat: RefreshToken 작성

---
+++ backend/src/main/java/com/ssafy/fittapet/backend/domain/entity/RefreshToken.java
@@ -0,0 +1,20 @@
+package com.ssafy.fittapet.backend.domain.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+@RedisHash(value = "refreshToken")
+@Getter
+@Builder
+public class RefreshToken {
+
+    @Id
+    private Long userId;
+    private String token;
+
+    @TimeToLive
+    private Long expiration;
+}
