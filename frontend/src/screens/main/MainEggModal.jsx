0a05b3d483af80a212f6bbdd37a93e542a442bf4
장성일 <cart1231@naver.com>
2024-11-07 13:19:35 +0900

    feat: 알받기 모달

---
+++ frontend/src/screens/main/MainEggModal.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from 'react';
+import { View, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
+import CustomModal from '@components/CustomModal/CustomModal';
+import CustomText from '@components/CustomText/CustomText';
+import CustomButton from '@components/CustomButton/CustomButton';
+import { colors } from '@src/constants';
+
+const MainEggModal = ({ isVisible, onClose }) => {
+  const [step, setStep] = useState(1);
+  const [petName, setPetName] = useState('');
+
+  const handleNextStep = () => {
+    setStep(step + 1);
+  };
+
+  const handleDoubleStep = () => {
+    setStep(step + 2);
+  };
+
+  const handleReset = () => {
+    setStep(1);
+    setPetName('');
+    onClose();
+  };
+
+  return (
+    <CustomModal
+      title="알을 받아봐요!"
+      isVisible={isVisible}
+      onClose={handleReset}
+    >
+      {step === 1 && (
+        <>
+          <View style={styles.bodyContainer}>
+            <CustomText style={styles.bodyText}>처음 오셨네요!</CustomText>
+            <CustomText>버튼을 눌러 알을 받아봐요!</CustomText>
+          </View>
+          <View style={styles.buttonContainer}>
+            <CustomButton title="알 줘" onPress={handleNextStep} />
+          </View>
+        </>
+      )}
+
+      {step === 2 && (
+        <>
+          <View style={styles.bodyContainer}>
+            <CustomText style={styles.bodyText}>알이 왔네요!</CustomText>
+            <CustomText>이름을 지어주실래요?</CustomText>
+          </View>
+          <View style={styles.twoButtonContainer}>
+            <CustomButton title="다음에" onPress={handleDoubleStep} />
+            <CustomButton title="좋아" onPress={handleNextStep} />
+          </View>
+        </>
+      )}
+
+      {step === 3 && (
+        <>
+          <View style={styles.bodyContainer}>
+            <CustomText style={styles.bodyText}>좋아요!</CustomText>
+            <CustomText style={styles.bodyText}>이름은 뭐로 할까요?</CustomText>
+
+            <TextInput
+              style={styles.input}
+              placeholder="내 펫의 이름은..."
+              value={petName}
+              onChangeText={setPetName}
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <CustomButton title="이거야!" onPress={handleNextStep} />
+          </View>
+        </>
+      )}
+
+      {step === 4 && (
+        <>
+          <View style={styles.bodyContainer}>
+            <CustomText style={styles.bodyText}>이제 퀘스트를 통해</CustomText>
+            <CustomText>당신의 친구를 성장시켜줘요!</CustomText>
+          </View>
+          <View style={styles.buttonContainer}>
+            <CustomButton title="그래!" onPress={handleReset} />
+          </View>
+        </>
+      )}
+    </CustomModal>
+  );
+};
+
+const styles = StyleSheet.create({
+  bodyContainer: { width: 280, justifyContent: 'center' },
+  bodyText: {
+    marginBottom: 5,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+  },
+
+  twoButtonContainer: {
+    marginTop: 20,
+    width: 250,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+  },
+  input: {
+    fontFamily: 'DungGeunMo',
+    fontSize: 16,
+    backgroundColor: colors.BACKGROUND_COLOR,
+    paddingLeft: 15,
+    borderRadius: 20,
+  },
+});
+
+export default MainEggModal;
