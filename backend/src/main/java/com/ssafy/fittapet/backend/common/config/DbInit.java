00d293ee287e2ef8490da5270ae55caae00f6c3d
yootal <khyoo96@naver.com>
2024-11-15 17:19:36 +0900

    Fix: DbInit 퀘스트 등록 수정

--- backend/src/main/java/com/ssafy/fittapet/backend/common/config/DbInit.java
+++ backend/src/main/java/com/ssafy/fittapet/backend/common/config/DbInit.java
@@ -29,7 +29,27 @@ public class DbInit {
     @PostConstruct
     void init() {
 
-        if(userRepository.count() == 0) {
+
+        if (questRepository.count() == 0) {
+            /* 퀘스트 더미데이터 생성 */
+            List<Quest> quests = new ArrayList<>();
+            long reward = 100L;
+
+            for (QuestTier questTier : QuestTier.values()) {
+                for (QuestType questType : QuestType.values()) {
+                    for (QuestCategory questCategory : QuestCategory.values()) {
+                        String questName = questTier.getValue() + "_" + questType.getValue() + "_" + questCategory.getName();
+                        String questContent = questType.getValue() + questCategory.getName();
+
+                        Quest quest = createQuest(questName, questContent, questTier, questType, questCategory, reward);
+                        quests.add(quest);
+                    }
+                }
+            }
+            questRepository.saveAll(quests);
+        }
+
+        if (userRepository.count() == 0) {
             /* 유저 더미데이터 생성 */
             List<User> users = new ArrayList<>();
 
@@ -46,43 +66,29 @@ public class DbInit {
             users.add(user5);
 
             userRepository.saveAll(users);
-        }
 
-        if(questRepository.count() == 0) {
-            /* 퀘스트 더미데이터 생성 */
-            List<Quest> quests = new ArrayList<>();
-            long reward = 100L;
-
-            for(QuestTier questTier : QuestTier.values()) {
-                for(QuestType questType : QuestType.values()) {
-                    for(QuestCategory questCategory : QuestCategory.values()) {
-                        String questName = questTier.getValue() + "_" + questType.getValue() + "_" + questCategory.getName();
-                        String questContent = questType.getValue()+questCategory.getName();
-
-                        Quest quest = createQuest(questName, questContent, questTier, questType, questCategory, reward);
-                        quests.add(quest);
-                    }
-                }
-            }
-            questRepository.saveAll(quests);
-        }
-
-        if(personalQuestRepository.count() == 0) {
-            /* 개인 퀘스트 더미데이터 생성 */
-            List<Quest> personalQuests = questRepository.findAllByQuestType(QuestType.PERSONAL);
-            User user = userRepository.findById(1L).orElse(null);
-
-            for(Quest quest : personalQuests) {
-                PersonalQuest personalQuest = PersonalQuest.builder()
-                        .quest(quest)
-                        .user(user)
-                        .questStatus(quest.getId() % 3 != 0)
-                        .build();
-                personalQuestRepository.save(personalQuest);
+            if (personalQuestRepository.count() == 0) {
+                /* 개인 퀘스트 더미데이터 생성 */
+//                List<Quest> personalQuests = questRepository.findAllByQuestType(QuestType.PERSONAL);
+//                User user = userRepository.findById(1L).orElse(null);
+//
+//                for(Quest quest : personalQuests) {
+//                    PersonalQuest personalQuest = PersonalQuest.builder()
+//                            .quest(quest)
+//                            .user(user)
+//                            .questStatus(quest.getId() % 3 != 0)
+//                            .build();
+//                    personalQuestRepository.save(personalQuest);
+//                }
+                addPersonalQuests(user1);
+                addPersonalQuests(user2);
+                addPersonalQuests(user3);
+                addPersonalQuests(user4);
+                addPersonalQuests(user5);
             }
         }
 
-        if(guildRepository.count() == 0) {
+        if (guildRepository.count() == 0) {
             guildRepository.save(
                     Guild.builder().guildLeader(userRepository.findById(3L).orElse(null))
                             .guildName("Init Guild")
@@ -128,5 +134,21 @@ public class DbInit {
                 .questCategory(questCategory)
                 .questReward(questReward)
                 .build();
+    }
+
+    private void addPersonalQuests(User user) {
+
+        List<Quest> quests = questRepository.findAll();
+        List<PersonalQuest> personalQuests = quests.stream()
+                .map(quest ->
+                        PersonalQuest.builder()
+                                .user(user)
+                                .quest(quest)
+                                .questStatus(false)
+                                .build()
+                )
+                .toList();
+
+        personalQuestRepository.saveAll(personalQuests);
     }
 }
