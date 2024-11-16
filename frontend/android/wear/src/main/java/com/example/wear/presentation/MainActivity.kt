927c996cef32f010027f3b1d624df9719c42ccb3
yootal <khyoo96@naver.com>
2024-11-16 22:40:22 +0900

    Feat: watch

---
+++ frontend/android/wear/src/main/java/com/example/wear/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wear.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.wear.R
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
