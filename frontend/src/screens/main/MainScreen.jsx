fc6199cf44b9c33fc3d9945e31a798de06c8b035
장성일 <cart1231@naver.com>
2024-11-16 18:34:58 +0900

    feat: 알 획득 모달 로직 회원가입 부분 완성

--- frontend/src/screens/main/MainScreen.jsx
+++ frontend/src/screens/main/MainScreen.jsx
@@ -30,7 +30,8 @@ import { useMainPetInfo, useUpdateNickname } from '@hooks/queries/usePet';
 import AnimatedSprite from '@components/AnimatedSprite/AnimatedSprite';
 import { fetchHealthData } from '@api/healthData';
 import useHealthDataStore from '@src/stores/healthDataStore';
-
+import MainEggModal from './MainEggModal';
+import useEggModalDataStore from '@src/stores/eggModalDataStore';
 function MainScreen({ navigation }) {
   const [isModalVisible, setModalVisible] = useState(false);
   const [petNickname, setPetNickname] = useState('');
@@ -38,6 +39,21 @@ function MainScreen({ navigation }) {
   const { data: mainPetInfo, isLoading, isError, error } = useMainPetInfo();
   const { mutate } = useUpdateNickname();
   const { steps, sleepHours, updateHealthData } = useHealthDataStore();
+
+  const { shouldShowModal, newPetType, newPetStatus, setEggModalData } =
+    useEggModalDataStore();
+  const [isEggModalVisible, setEggModalVisible] = useState(false); // 에그모달 상태
+  const [step, setStep] = useState(1); // 에그모달의 단계 관리
+
+  useEffect(() => {
+    // 이후 params 변경 감지
+    if (shouldShowModal) {
+      console.log('shouldShowModal is true');
+      setEggModalVisible(true);
+    } else {
+      setEggModalVisible(false);
+    }
+  }, [shouldShowModal]);
 
   // 메인펫 정보가 바뀌면 업데이트
   useEffect(() => {
@@ -70,16 +86,13 @@ function MainScreen({ navigation }) {
   if (isLoading) {
     return <ActivityIndicator size="large" color={colors.MAIN_GREEN} />;
   }
-  if (isError) {
-    return <Text>Error occurred: {error.message}</Text>;
-  }
 
   // const petImage =
   //   // 대표 이미지 로딩
   //   petImages[mainPetInfo.petType]?.[mainPetInfo.petStatus] || null;
 
   const petSpriteImage =
-    petSpriteImages[mainPetInfo.petType]?.[mainPetInfo.petStatus] || null;
+    petSpriteImages[mainPetInfo?.petType]?.[mainPetInfo?.petStatus] || null;
 
   const petSpriteData = require('@assets/pets/sprite/sprite.json');
 
@@ -106,6 +119,31 @@ function MainScreen({ navigation }) {
     // 적당히 조절 해야할듯, 아님 동물마다 저장을 하던가
   };
 
+  // 펫북아이디 아니라 회원가입 및 주는 팻 id 값으로 하기
+  const handleEggUpdateNickname = async () => {
+    if (petNickname.trim()) {
+      try {
+        mutate({ petBookId, newNickname: petNickname.trim() });
+        setStep(3); // Step3로 이동
+      } catch (error) {
+        console.error('닉네임 업데이트 실패:', error);
+      }
+    } else {
+      Alert.alert('Error', '닉네임을 입력해주세요!');
+    }
+  };
+
+  const handleEggModalClose = async () => {
+    setEggModalVisible(false);
+
+    // Zustand 상태 업데이트
+    setEggModalData({ shouldShowModal: false });
+
+    setTimeout(() => {
+      setStep(1);
+    }, 300); // 300ms 딜레이
+  };
+
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -113,6 +151,19 @@ function MainScreen({ navigation }) {
         style={styles.backgroundImage}
         resizeMode="cover"
       >
+        {/* 닉네임은 새걸 받아온걸로 하기 */}
+        <MainEggModal
+          isVisible={isEggModalVisible}
+          step={step}
+          petNickname={petNickname}
+          setPetNickname={setPetNickname}
+          onUpdateNickname={handleEggUpdateNickname}
+          onClose={handleEggModalClose}
+          setStep={setStep}
+          // newPetType={newPetType} // 추가
+          // newPetStatus={newPetStatus}
+        />
+
         <Text>{steps}</Text>
         <Text>{sleepHours}</Text>
         {/* 상단 - 레벨 및 진행 상태 */}
