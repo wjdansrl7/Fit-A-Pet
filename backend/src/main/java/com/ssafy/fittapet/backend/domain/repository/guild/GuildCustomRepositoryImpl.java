c27fb43eefd6f142ad40cd84c3f70c91f5a88eff
yootal <khyoo96@naver.com>
2024-11-18 17:03:54 +0900

    Fix: 길드 탈퇴 수정6

--- backend/src/main/java/com/ssafy/fittapet/backend/domain/repository/guild/GuildCustomRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/fittapet/backend/domain/repository/guild/GuildCustomRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.ssafy.fittapet.backend.domain.repository.guild;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.fittapet.backend.domain.dto.guild.GuildInfoResponse;
 import com.ssafy.fittapet.backend.domain.dto.guild.GuildMemberInfoResponse;
@@ -57,6 +59,10 @@ public class GuildCustomRepositoryImpl implements GuildCustomRepository {
         QPetBook petBook = QPetBook.petBook;
         QUserQuestStatus userQuestStatus = QUserQuestStatus.userQuestStatus;
 
+        BooleanExpression userQuestStatusCondition = userQuestStatus.guildQuest != null
+                ? userQuestStatus.guildQuest.guild.id.eq(guildId)
+                : null;
+
         return queryFactory
                 .selectDistinct(new QGuildMemberInfoResponse(
                         map.user.id,
@@ -69,7 +75,7 @@ public class GuildCustomRepositoryImpl implements GuildCustomRepository {
                 .leftJoin(userQuestStatus).on(userQuestStatus.user.eq(map.user))
                 .where(map.guild.id.eq(guildId)
                         .and(petBook.id.eq(map.user.petMainId))
-                        .and((userQuestStatus.guildQuest.guild.id).eq(guildId)))
+                        .and(userQuestStatusCondition != null ? userQuestStatusCondition : Expressions.FALSE))
                 .fetch();
     }
 }
